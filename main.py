f9dbf95e7462203bf1601f1eb0f3ec0598ebe299
ackwolver335 <abhaych335@gmail.com>
2024-07-15 22:20:14 +0530

    main code file added

---
+++ main.py
@@ -0,0 +1,35 @@
+import tkinter as tk 
+import calendar as cd 
+import tkinter.messagebox as mg
+
+w1 =  tk.Tk()
+w1.title('Calendar Generator')
+w1.geometry('550x600')
+w1.resizable(False,False)
+
+frm1 = tk.Frame(w1)
+frm1.pack()
+
+lb1 = tk.Label(frm1,text = 'Calendar Generator',font = ('Fira Code',12,'bold'))
+lb1.pack(padx = 2,pady = 3)
+
+lb2 = tk.Label(frm1,text = 'Provide Year of Calendar',font = ('Fira Code',8))
+lb2.pack(padx = 3,pady = 5)
+
+e1 = tk.Entry(frm1)
+e1.pack(padx = 2,pady = 3)
+
+def method1():
+    if e1.get().isdigit():
+        yoc = int(e1.get())
+        caldr1 = cd.Day(yoc)
+
+        lb3.config(text = caldr1)
+
+btn1 = tk.Button(w1,text = 'Generate Calendar',font = ('Fira Code',10,'bold'),command = method1)
+btn1.pack()
+
+lb3 = tk.Label(w1,text = 'Calendar Generated')
+lb3.pack(padx = 5)
+
+w1.mainloop()
