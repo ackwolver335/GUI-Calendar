95647c3f4b17772ebe0dfaf38587f50a5b9be1bc
ackwolver335 <abhaych335@gmail.com>
2024-07-19 19:45:57 +0530

    Main Source Code File update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as mg
 
 w1 =  tk.Tk()
 w1.title('Calendar Generator')
-w1.geometry('550x600')
+w1.geometry('250x130')
 w1.resizable(False,False)
 
 frm1 = tk.Frame(w1)
@@ -17,18 +17,28 @@ lb2 = tk.Label(frm1,text = 'Provide Year of Calendar',font = ('Fira Code',8))
 lb2.pack(padx = 3,pady = 5)
 
 e1 = tk.Entry(frm1)
-e1.pack(padx = 2,pady = 3)
+e1.pack(padx = 3,pady = 3)
 
 def method1():
-    yoc = int(e1.get())
-    caldr1 = cd.calendar(yoc)
 
-    lb3.config(text = caldr1,font = "Consolas 8 bold")
+    # main calendar window code
+    if len(e1.get()) > 4 or len(e1.get()) < 4:
+        mg1 = mg.showwarning('Application Warning','Year length is not appropriate !')
 
-btn1 = tk.Button(w1,text = 'Generate Calendar',font = ('Fira Code',10,'bold'),command = method1)
-btn1.pack()
+    else:
+        # new window for calendar
+        w2 = tk.Toplevel(w1)
+        w2.title('Calendar of ' + e1.get())
+        w2.geometry('550x480')
 
-lb3 = tk.Label(w1,text = 'Calendar Generated')
-lb3.pack(padx = 5)
+        # main code data
+        yoc = int(e1.get())
+
+        caldr = cd.calendar(yoc)
+        lb1 = tk.Label(w2,text = caldr,font = "Consolas 8 bold")
+        lb1.pack(padx = 2,pady = 3)
+
+btn1 = tk.Button(w1,text = 'Generate Calendar',font = ('Fira Code',10,'bold'),command = method1,relief = 'groove')
+btn1.pack(pady = 5)
 
 w1.mainloop()
