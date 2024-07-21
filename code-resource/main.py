7012194557bd7ef68cc90d14e0ac5f3bcf759d8a
ackwolver335 <abhaych335@gmail.com>
2024-07-21 11:27:36 +0530

    Main code resource file added

--- code-resource/main.py
+++ code-resource/main.py
@@ -6,14 +6,15 @@ w1 =  tk.Tk()
 w1.title('Calendar Generator')
 w1.geometry('250x130')
 w1.resizable(False,False)
+w1.config(background = "#292826")
 
-frm1 = tk.Frame(w1)
+frm1 = tk.Frame(w1,bg = "#292826")
 frm1.pack()
 
-lb1 = tk.Label(frm1,text = 'Calendar Generator',font = ('Fira Code',12,'bold'))
+lb1 = tk.Label(frm1,text = 'Calendar Generator',font = ('Fira Code',10,'bold'),fg = "#F9D342",bg = "#292826")
 lb1.pack(padx = 2,pady = 3)
 
-lb2 = tk.Label(frm1,text = 'Provide Year of Calendar',font = ('Fira Code',8))
+lb2 = tk.Label(frm1,text = 'Provide Year of Calendar',font = ('Fira Code',8),fg = "#F9D342",bg = "#292826")
 lb2.pack(padx = 3,pady = 5)
 
 e1 = tk.Entry(frm1)
@@ -30,15 +31,16 @@ def method1():
         w2 = tk.Toplevel(w1)
         w2.title('Calendar of ' + e1.get())
         w2.geometry('550x480')
+        w2.config(bg = '#292826')
 
         # main code data
         yoc = int(e1.get())
 
         caldr = cd.calendar(yoc)
-        lb1 = tk.Label(w2,text = caldr,font = "Consolas 8 bold")
+        lb1 = tk.Label(w2,text = caldr,font = "Consolas 8 bold",fg = "#F9D342",bg = "#292826")
         lb1.pack(padx = 2,pady = 3)
 
-btn1 = tk.Button(w1,text = 'Generate Calendar',font = ('Fira Code',10,'bold'),command = method1,relief = 'groove')
+btn1 = tk.Button(w1,text = 'Generate Calendar',font = ('Fira Code',10,'bold'),command = method1,relief = 'groove',fg = "#F9D342",bg = "#292826")
 btn1.pack(pady = 5)
 
 w1.mainloop()
